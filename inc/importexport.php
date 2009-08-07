c3d77da5fba55f61010a1e62effddfdc5ea6f378
dbu <david.buchmann@liip.ch>
2009-08-07 17:05:03 +0200

    added export method

--- inc/importexport.php
+++ inc/importexport.php
@@ -4,28 +4,28 @@
  * the java binary
  */
 class jackalope_importexport {
-    
+
     protected $fixturePath;
     protected $jar;
-    
+
     public function __construct($path) {
         $this->fixturePath = dirname(__FILE__) . '/../fixtures/' . $path . '/';
         if (!is_dir($this->fixturePath)) {
             throw new Exception('Not a valid directory: ' . $this->fixturePath);
         }
-        
+
         $this->jar = dirname(__FILE__) . '/../bin/jack.jar';
         if (!file_exists($this->jar)) {
             throw new Exception('jack.jar not found at: ' . $this->jar);
         }
     }
-    
+
     public function import($fixture) {
         $fixture = $this->fixturePath . $fixture;
         if (!is_readable($fixture)) {
             throw new Exception('Fixture not found at: ' . $fixture);
         }
-        
+
         //TODO fix the stderr redirect which doesn't work properly
         exec('java -jar ' . $this->jar . ' import ' . $fixture . " 2>&1",$output,  $ret);
         if ($ret !== 0){
@@ -37,4 +37,21 @@ class jackalope_importexport {
         }
         return true;
     }
-}
+
+    public function exportdocument($file) {
+        $fixture = $this->fixturePath . $file;
+        if (is_readable($fixture)) {
+            throw new Exception('File existing: ' . $fixture);
+        }
+
+        //TODO fix the stderr redirect which doesn't work properly
+        exec('java -jar ' . $this->jar . ' exportdocument ' . $fixture . " 2>&1",$output,  $ret);
+        if ($ret !== 0){
+            $msg = '';
+            foreach ($output as $line) {
+                $msg .= $line . "\n";
+            }
+            throw new Exception($msg);
+        }
+        return true;
+    }}
