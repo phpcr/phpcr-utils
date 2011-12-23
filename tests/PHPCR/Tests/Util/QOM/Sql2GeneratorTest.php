8651b3b581e850da974544b1313e252a02131153
Fabian Vogler <fabian@equivalence.ch>
2011-12-23 17:13:10 +0100

    Tests for evalLiteral

---
+++ tests/PHPCR/Tests/Util/QOM/Sql2GeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PHPCR\Tests\Util\QOM;
+
+use PHPCR\Util\QOM\Sql2Generator;
+
+class Sql2GeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $generator;
+
+    public function setUp()
+    {
+        $this->generator = new Sql2Generator();
+    }
+
+    public function testLiteral()
+    {
+        $literal = $this->generator->evalLiteral('Foobar');
+        $this->assertEquals("'Foobar'", $literal);
+    }
+
+    public function testDateTimeLiteral()
+    {
+        $literal = $this->generator->evalLiteral(new \DateTime('2011-12-23'));
+        $this->assertEquals("CAST('2011-12-23T00:00:00.000+01:00' AS DATE)", $literal);
+    }
+}
