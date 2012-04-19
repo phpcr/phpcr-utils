d8742977a00be81acae2218e98aacdc4d6c7d97a
Christian Stocker <me@chregu.tv>
2012-04-19 20:19:37 +0200

    Added more tests

--- tests/PHPCR/Tests/Util/QOM/Sql1GeneratorTest.php
+++ tests/PHPCR/Tests/Util/QOM/Sql1GeneratorTest.php
@@ -42,4 +42,44 @@ class Sql1GeneratorTest extends \PHPUnit_Framework_TestCase
         $literal = $this->generator->evalLiteral(11.0);
         $this->assertSame("11.0", $literal);
     }
+
+    public function testChildNode()
+    {
+        $literal = $this->generator->evalChildNode("/foo/bar/baz");
+        $this->assertSame("jcr:path LIKE '/foo[%]/bar[%]/baz[%]/%' AND NOT jcr:path LIKE '/foo[%]/bar[%]/baz[%]/%/%'", $literal);
+    }
+
+    public function testDescendantNode()
+    {
+        $literal = $this->generator->evalDescendantNode("/foo/bar/baz");
+        $this->assertSame("jcr:path LIKE '/foo[%]/bar[%]/baz[%]/%'", $literal);
+    }
+
+    public function testPopertyExistence()
+    {
+        $literal = $this->generator->evalPropertyExistence(null, "foo");
+        $this->assertSame("foo IS NOT NULL", $literal);
+    }
+
+    public function testFullTextSearch()
+    {
+        $literal = $this->generator->evalFullTextSearch(null, "'foo'");
+        $this->assertSame("CONTAINS(*, 'foo')", $literal);
+        $literal = $this->generator->evalFullTextSearch(null, "'foo'", "bar");
+        $this->assertSame("CONTAINS(bar, 'foo')", $literal);
+    }
+
+    public function testColumns()
+    {
+        $literal = $this->generator->evalColumns(null);
+        $this->assertSame("s", $literal);
+        $literal = $this->generator->evalColumns(array("bar","foo"));
+        $this->assertSame("bar, foo", $literal);
+    }
+
+    public function testPropertyValue()
+    {
+        $literal = $this->generator->evalPropertyValue("foo");
+        $this->assertSame("foo", $literal);
+    }
 }
