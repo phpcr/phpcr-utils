6ab38f138cb582c054f147659b75046ca6360637
Daniel Barsotti <daniel.barsotti@liip.ch>
2012-09-19 13:42:49 +0200

    Work in progress: CND parser

--- src/PHPCR/Util/CND/Helper/CndSyntaxTreeNodeVisitor.php
+++ src/PHPCR/Util/CND/Helper/CndSyntaxTreeNodeVisitor.php
@@ -2,14 +2,13 @@
 
 namespace PHPCR\Util\CND\Helper;
 
-// TODO: this class needs to be rewritten using PHPCR\NodeType\NodeTypeManagerInterface to generate the node types
-// TODO: as it is now, this will not work !!!
-
 use PHPCR\Util\CND\Parser\SyntaxTreeNode,
     PHPCR\Util\CND\Parser\SyntaxTreeVisitorInterface,
     PHPCR\NamespaceRegistryInterface,
     PHPCR\NodeType\NodeTypeManagerInterface,
-    PHPCR\NodeType\NodeTypeDefinitionInterface;
+    PHPCR\NodeType\NodeTypeDefinitionInterface,
+    PHPCR\PropertyType,
+    PHPCR\Version\OnParentVersionAction;
 
 /**
  * @author Daniel Barsotti <daniel.barsotti@liip.ch>
@@ -17,7 +16,7 @@ use PHPCR\Util\CND\Parser\SyntaxTreeNode,
 class CndSyntaxTreeNodeVisitor implements SyntaxTreeVisitorInterface
 {
     /**
-     * @var \PHPCR\NodeTypeManagerInterface
+     * @var \PHPCR\NodeType\NodeTypeManagerInterface
      */
     protected $nodeTypeManager;
 
@@ -37,9 +36,14 @@ class CndSyntaxTreeNodeVisitor implements SyntaxTreeVisitorInterface
     protected $namespaces = array();
 
     /**
-     * @var \PHPCR\NodeTypeTemplateInterface
+     * @var \PHPCR\NodeType\NodeTypeTemplateInterface
      */
     protected $curNodeTypeDef;
+
+    /**
+     * @var \PHPCR\NodeType\PropertyDefinitionTemplateInterface
+     */
+    protected $curPropTypeDef;
 
     public function __construct(NamespaceRegistryInterface $nsRegistry, NodeTypeManagerInterface $ntManager)
     {
@@ -59,8 +63,6 @@ class CndSyntaxTreeNodeVisitor implements SyntaxTreeVisitorInterface
 
     public function visit(SyntaxTreeNode $node)
     {
-        var_dump($node->getType());
-
         switch ($node->getType()) {
 
             case 'nsMapping':
@@ -85,14 +87,82 @@ class CndSyntaxTreeNodeVisitor implements SyntaxTreeVisitorInterface
                 break;
 
             case 'nodeTypeAttributes':
-                if ($node->hasChild('orderable')) $this->curNodeTypeDef->setOrderableChildNodes(true);
-                if ($node->hasChild('mixin')) $this->curNodeTypeDef->setMixin(true);
-                if ($node->hasChild('abstract')) $this->curNodeTypeDef->setAbstract(true);
-                if ($node->hasChild('query')) $this->curNodeTypeDef->setQueryable(true);
+                $this->setNodeTypeAttributes($node);
+                // OPTIMIZATION: Here we could "ask" the tree not to send the children as they just have been taken in account
                 break;
 
-            // TODO: write the rest
+            case 'propertyDef':
+                $this->curPropTypeDef = $this->nodeTypeManager->createPropertyDefinitionTemplate();
+                $this->curNodeTypeDef->getPropertyDefinitionTemplates()->append($this->curPropTypeDef);
+                break;
+
+            case 'propertyName':
+                $this->curPropTypeDef->setName($node->getProperty('value'));
+                break;
+
+            case 'propertyType':
+                $this->curPropTypeDef->setRequiredType(PropertyType::valueFromName($node->getProperty('value')));
+                break;
+
+            case 'defaultValues':
+                $this->curPropTypeDef->setDefaultValues($node->getProperty('value'));
+                break;
+
+            case 'valueConstraints':
+                $this->curPropTypeDef->setValueConstraints($node->getProperty('value'));
+                break;
+
+            case 'propertyTypeAttributes':
+                $this->setPropertyTypeAttributes($node);
+                // OPTIMIZATION: Here we could "ask" the tree not to send the children as they just have been taken in account
+                break;
+
+            default:
+//                var_dump(sprintf('Unhandled node [%s]', $node->getType()));
+
         }
+    }
+
+    protected function setNodeTypeAttributes(SyntaxTreeNode $node)
+    {
+        if ($node->hasChild('orderable')) $this->curNodeTypeDef->setOrderableChildNodes(true);
+        if ($node->hasChild('mixin')) $this->curNodeTypeDef->setMixin(true);
+        if ($node->hasChild('abstract')) $this->curNodeTypeDef->setAbstract(true);
+        if ($node->hasChild('query')) $this->curNodeTypeDef->setQueryable(true);
+    }
+
+    protected function setPropertyTypeAttributes(SyntaxTreeNode $node)
+    {
+        if ($node->hasChild('autocreated')) $this->curPropTypeDef->setAutoCreated(true);
+        if ($node->hasChild('mandatory')) $this->curPropTypeDef->setMandatory(true);
+        if ($node->hasChild('protected')) $this->curPropTypeDef->setProtected(true);
+        if ($node->hasChild('multiple')) $this->curPropTypeDef->setMultiple(true);
+        if ($node->hasChild('queryops')) $this->curPropTypeDef->setAvailableQueryOperators($node->getPRoperty('value'));
+
+        if ($node->hasChild('nofulltext')) {
+            $this->curPropTypeDef->setFullTextSearchable(false);
+        } else {
+            // TODO: Check the property should be set to true when there is no nofulltext node
+            $this->curPropTypeDef->setFullTextSearchable(true);
+        }
+
+        if ($node->hasChild('noqueryorder')) {
+            $this->curPropTypeDef->setQueryOrderable(false);
+        } else {
+            // TODO: Check the property should be set to true when there is no noqueryorder node
+            $this->curPropTypeDef->setQueryOrderable(true);
+        }
+
+        // WARNING:
+        // Potentially the syntax tree could have more than one OPV node, which is wrong in a CND.
+        // If this happens the order of the "if" below determine which one will be used.
+        if ($node->hasChild('COPY')) $this->curPropTypeDef->setOnParentVersion(OnParentVersionAction::COPY);
+        if ($node->hasChild('VERSION')) $this->curPropTypeDef->setOnParentVersion(OnParentVersionAction::VERSION);
+        if ($node->hasChild('INITIALIZE')) $this->curPropTypeDef->setOnParentVersion(OnParentVersionAction::INITIALIZE);
+        if ($node->hasChild('COMPUTE')) $this->curPropTypeDef->setOnParentVersion(OnParentVersionAction::COMPUTE);
+        if ($node->hasChild('IGNORE')) $this->curPropTypeDef->setOnParentVersion(OnParentVersionAction::IGNORE);
+        if ($node->hasChild('ABORT')) $this->curPropTypeDef->setOnParentVersion(OnParentVersionAction::ABORT);
+        // TODO: What to do with an "OPV" node?
     }
 
 }
