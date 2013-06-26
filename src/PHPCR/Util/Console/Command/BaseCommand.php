f5a5faa3d8016d9500670aaf307ee9f32e9ba9cb
David Buchmann <david.buchmann@liip.ch>
2013-06-26 14:02:49 +0200

    properly use the cli helper

--- src/PHPCR/Util/Console/Command/BaseCommand.php
+++ src/PHPCR/Util/Console/Command/BaseCommand.php
@@ -4,14 +4,16 @@ namespace PHPCR\Util\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use PHPCR\Util\Console\Helper\PhpcrCliHelper;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use PHPCR\SessionInterface;
+use PHPCR\Util\Console\Helper\PhpcrCliHelper;
 
 abstract class BaseCommand extends Command
 {
     /**
-     * @return PHPCR\SessionInterface
+     * @return SessionInterface
      */
     protected function getPhpcrSession()
     {
@@ -19,12 +21,11 @@ abstract class BaseCommand extends Command
     }
 
     /**
-     * @return PHPCR\Util\Console\Helper\PhpcrCliHelper
+     * @return PhpcrCliHelper
      */
     protected function getPhpcrCliHelper()
     {
-        $phpcrCliHelper = new PhpcrCliHelper($this->getPhpcrSession());
-        return $phpcrCliHelper;
+        return $this->getHelper('phpcr_cli');
     }
 
     public function configureNodeManipulationInput()
