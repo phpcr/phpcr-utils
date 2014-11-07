cfa0bc48a05678a479815e3496c9b008c852a66d
David ALLIX <webda2l@gmail.com>
2014-11-07 13:45:31 +0100

    Fix array_search check

--- src/PHPCR/Util/Console/Command/WorkspaceCreateCommand.php
+++ src/PHPCR/Util/Console/Command/WorkspaceCreateCommand.php
@@ -60,7 +60,7 @@ EOT
             return 1;
         }
 
-        if (array_search($workspaceName, $workspace->getAccessibleWorkspaceNames())) {
+        if (false !== array_search($workspaceName, $workspace->getAccessibleWorkspaceNames())) {
             $output->writeln(
                 sprintf('<comment>This repository already has a workspace called "%s"</comment>', $workspaceName)
             );
